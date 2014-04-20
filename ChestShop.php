d9e39a97b4afb8fcae9b4e3f47be9a044229f85e
MinecrafterJPN <minecrafter.mn@gmail.com>
2014-04-20 11:07:25 +0900

    id changes to name automatically

--- ChestShop.php
+++ ChestShop.php
@@ -75,10 +75,10 @@ class ChestShop implements Plugin
 					if ($pID === false) break;
 					$chest = $this->getSideChest($data);
 					if ($chest === false) break;
-					//if (strlen($shopOwner) > 15) $shopOwner = substr($shopOwner, 0, 15);
-
-					$data->setText($shopOwner, "Amount:$saleNum", "Price:$price", $data->data['Text4']);
+					$producName = isset(Block::$class[$pID]) ? Block::$class[$pID] : Item::$class[$pID];
+					$data->setText($shopOwner, "Amount:$saleNum", "Price:$price", "$productName:$pMeta");
 					// 自動変換処理
+
 					$this->db->exec("INSERT INTO ChestShop (shopOwner, saleNum, price, productID, productMeta, signX, signY, signZ, chestX, chestY, chestZ) VALUES ('$shopOwner', $saleNum, $price, $pID, $pMeta, $data->x, $data->y, $data->z, $chest->x, $chest->y, $chest->z)");
 				}
 				break;
